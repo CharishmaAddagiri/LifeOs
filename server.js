d1eee27320d4721125598bda1e823d64ead7a8dd
Sai charishma addagiri <charishmaaddagiri2007@gmail.com>
2026-05-24 19:44:21 +0530

    made a correction in the deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app     = express()
 app.use(express.static(__dirname))
 app.get("/", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "dashboard.html")
+    path.join(__dirname, "login.html")
   )
 })
 
